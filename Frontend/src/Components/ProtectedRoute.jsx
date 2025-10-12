6f16041b90a2a0ce4174afd1405dc3b003259cc3
Shashank-9676 <shashankchowdary09@gmail.com>
2025-10-12 19:59:32 +0530

    fix: Update ProtectedRoute to retrieve user details from localStorage instead of Cookies

--- Frontend/src/Components/ProtectedRoute.jsx
+++ Frontend/src/Components/ProtectedRoute.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Navigate } from 'react-router';
-import Cookies from 'js-cookie'; 
+
 const ProtectedRoute = ({ children }) => {
-    const userDetails = Cookies.get('jwt_token');
+    const userDetails = localStorage.getItem('userDetails');
     if (!userDetails) {
         return <Navigate to="/login" replace />;
     }
     return <div>
         {children}
+
     </div>
 };
 
